ea8f3c6897def99fcf87acda3bcbe23d662d7155
adipratamajaya <apratama272@gmail.com>
2023-08-20 17:16:53 +0700

    update

--- Example/asset/js/scriptMe.js
+++ Example/asset/js/scriptMe.js
@@ -1,5 +1,5 @@
 (function () {
-  var words = ["search", "build", "learning"];
+  var words = ["search", "build", "work","learning"];
   i = 0;
   setInterval(function(){ $('#kata_ganti').fadeOut(function(){
       $(this).html(words[(i = (i + 1) % words.length)]).fadeIn();
