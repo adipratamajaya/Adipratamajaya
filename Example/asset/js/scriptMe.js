aaa7070cabd1b067141051694bba09ff4eb1815c
adipratamajaya <apratama272@gmail.com>
2023-08-24 22:19:00 +0700

    update hamburger

--- Example/asset/js/scriptMe.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  var words = ["search", "build", "work","learning"];
-  i = 0;
-  setInterval(function(){ $('#kata_ganti').fadeOut(function(){
-      $(this).html(words[(i = (i + 1) % words.length)]).fadeIn();
-    }); }, 3000)
-})();
