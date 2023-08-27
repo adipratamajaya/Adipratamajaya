02ec32a4f65f13c97403204607dbbf38398d6304
adipratamajaya <apratama272@gmail.com>
2023-08-28 00:35:04 +0700

    Update

--- Example/asset/js/script.js
+++ Example/asset/js/script.js
@@ -1,4 +1,5 @@
-alert("website under display improvement 😝")
+// alert("website under display improvement 😝")
+
 const mamanRacing = document.querySelector("#nengDonat");
 const powerRaginang = document.querySelector(".modeNight");
 
@@ -6,10 +7,13 @@ powerRaginang.addEventListener("click", ()=> {
   const rangerMerah = document.querySelector('main');
   const birunya = document.querySelector("#kata_ganti");
   const navNya = document.querySelector("header");
+  const me = document.querySelector(".des_");
+
 
   rangerMerah.classList.toggle('night');
   birunya.classList.toggle('night');
   navNya.classList.toggle('night');
+  me.classList.toggle(`night`);
 })
 
 mamanRacing.addEventListener("click", () => {
