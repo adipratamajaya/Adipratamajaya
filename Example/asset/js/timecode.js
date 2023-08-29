7c759401eccfc0342f1ade5e7443989436cea151
adipratamajaya <apratama272@gmail.com>
2023-08-29 20:27:08 +0700

    delet web lama

--- Example/asset/js/timecode.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var words = ["Learning","bulid","create","search"],
-    i = 0;
-    setInterval(function(){ $('#kata_ganti').fadeOut(function(){
-        $(this).html(words[(i = (i + 1) % words.length)]).fadeIn();
-      }); }, 3000)
-  })();
