6d7ea4a3c317432391c4685c6b2ab2249587f736
adipratamajaya <apratama272@gmail.com>
2023-08-31 18:46:10 +0700

    update

--- apps/js/script.js
+++ apps/js/script.js
@@ -11,25 +11,11 @@ burgerking.addEventListener("click", () => {
 
 powerRaginang.addEventListener("click", ()=> {
 const bodyNight = document.querySelector("body");
-  const birunya = document.querySelector("#kata_ganti");
   const navNya = document.querySelector("header");
-  const me = document.querySelector(".des_");
-  const headernya = document.querySelector("footer");
-  const plusnya = document.querySelector(".plus_");
-  const projectNya = document.querySelector(".clinet");
-  const textProfile = document.querySelector(".text_profile");
-  const landing = document.querySelector(".landing__");
 
 
-  textProfile.classList.toggle("night");
-  landing.classList.toggle("night");
   bodyNight.classList.toggle('night');
-  birunya.classList.toggle('night');
   navNya.classList.toggle('night');
-  me.classList.toggle(`night`);
-  headernya.classList.toggle('night');
-  plusnya.classList.toggle('night');
-  projectNya.classList.toggle("night");
 })
 
 mamanRacing.addEventListener("click", () => {
